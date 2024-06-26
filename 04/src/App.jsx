47a75e8e28250051eff45e34814ddc0fef179115
= <=>
2024-06-26 22:56:32 +0300

    day 05 working on props

--- 04/src/App.jsx
+++ 04/src/App.jsx
@@ -1,11 +1,17 @@
 import Navbar from "./components/Navbar.jsx"
 import Hero from "./components/Hero.jsx"
+import profile from "./assets/man.jpg"
+import User from "./components/User.jsx"
 export default function App() {
   return (
     <>
       
-      <Navbar title="Hood"/>
-      <Hero />
+      {/* <Navbar title="Hood"/> */}
+      <User desc="Fullstack developer , Lets get you Wings on the internet"
+        username="Developer Profile"
+        stack={['Mongo DB', 'SQL', 'Redis', 'Nodejs', 'python', 'Django']}
+        img={profile}
+      />
     </>
   )
 }
---
+++ 04/src/components/User.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+/**
+ * 
+ * @param {*} props username, desc, stack, img
+ * 
+ * @returns 
+ */
+export default function User(props) {
+    const divStyle = {
+        maxWidth: "540px",
+        justifyContent: "center",
+        alignItems: "center",
+        margin: "auto", // This centers the card horizontally
+        display: "flex",
+        height:"100vh"
+    };
+    const smallImg = {
+        width:"25px",
+        height:"25px",
+        margin: "0 5px",
+        padding:"2px"
+    }
+    return (
+        <>
+            <div className="card mb-3" >
+                <div className="row g-0" style={divStyle}>
+                    <div className="col-md-4">
+                        <img src={props.img} className="img-responsive p-3" alt="profile" />
+                    </div>
+                    <div className="col-md-8 d-flex">
+                        <div className="card-body mt-4">
+                            <h5 className="card-title m-1 underline d-flex" >{props.username}
+                                <span><img src="https://upload.wikimedia.org/wikipedia/commons/3/3b/Eo_circle_green_checkmark.svg" style={smallImg}  alt="" /></span>
+                            </h5>
+                            <p className="card-text m-1">{props.desc}</p>
+                            <ul className="d-flex flex-wrap" style={{ listStyleType: "none" }}>
+                                {props.stack.map((item, index) => (
+                                    <li key={index} className="text-light bg-primary p-1 rounded m-1">{item}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
